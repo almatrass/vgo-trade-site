22f815d2eb2442bb266bc8e00d2d3f7e1e50eb99
almatrass <rosscraigbrown@hotmail.co.uk>
2018-10-29 21:26:40 +0000

    Fixed max inventory items

--- server/inventory.js
+++ server/inventory.js
@@ -28,56 +28,81 @@ function getBotInventory(cb) {
 	if (!cb) {
 		throw new Error('A callback function must be provided');
 	} else {
-		ET.IUser.GetInventory((err, body) => {
-			if (err) {
-				cb(err);
-			} else {
-				if (body.status == 1) {
-					let inventory = [];
-					// Loop through bot items
-					body.response.items.forEach(function(item) {
-						let banned;
-						// Check if the bot item is banned by us in config:
-						if (config.bannedBotItems.length) {
-							config.bannedBotItems.forEach(function(bannedItem) {
-								if (item.name.toUpperCase().includes(bannedItem.toUpperCase())) {
-									banned = true;
-								}
-							});
-						}
-						
-						let price = item.suggested_price;
-						
-						// Change the price based on the rates set in config
-						if (item.name == 'Skeleton Key') {
-							price = parseFloat(((price * config.rates.keyBotRate) / 100).toFixed(2));
-						} else {
-							price = parseFloat(((price * config.rates.otherBotRate) / 100).toFixed(2));
-						}
-						
-						
-						// Check if the item price is allowed in config
-						if (price >= config.minMax.minBot && price <= config.minMax.maxBot && !banned) {
-							// If all is good, push only the data we need to a local inventory array:
-							inventory.push({
-								name: item.name,
-								id: item.id,
-								category: item.category,
-								color: item.color,
-								img: item.image['600px'],
-								price: price,
-                rarity: item.rarity
-							});
-						}
-					});
-					// Call the callback, returning our nicely formatted inventory
-					cb(null, inventory);
-				} else {
-					// Status wasn't 1:
-					cb(new Error(body.message));
-				}
-			}
-		});
+    
+    let demItems = [];
+    let page = 1;
+    
+    getIt();
+    
+    function getIt() {
+      ET.IUser.GetInventory({
+        page: page,
+        per_page: 1
+      }, (err, body) => {
+        if (err) {
+          cb(err);
+        } else {
+          if (body.status == 1) {
+            let items = body.response.items;
+            items.forEach(function(item) {
+              demItems.push(item);
+            });
+
+            if (body.total_pages > body.current_page) {
+              page += 1;
+              getIt();
+            } else {
+              nextStep(demItems);
+            }
+          } else {
+            // Status wasn't 1:
+            cb(new Error(body.message));
+          }
+        }
+      });
+    }
+    
+    function nextStep(demItems) {
+      let inventory = [];
+      // Loop through bot items
+      demItems.forEach(function(item) {
+        let banned;
+        // Check if the bot item is banned by us in config:
+        if (config.bannedBotItems.length) {
+          config.bannedBotItems.forEach(function(bannedItem) {
+            if (item.name.toUpperCase().includes(bannedItem.toUpperCase())) {
+              banned = true;
+            }
+          });
+        }
+
+        let price = item.suggested_price;
+
+        // Change the price based on the rates set in config
+        if (item.name == 'WAX Key') {
+          price = parseFloat(((price * config.rates.keyBotRate) / 100).toFixed(2));
+        } else {
+          price = parseFloat(((price * config.rates.otherBotRate) / 100).toFixed(2));
+        }
+
+
+        // Check if the item price is allowed in config
+        if (price >= config.minMax.minBot && price <= config.minMax.maxBot && !banned) {
+          // If all is good, push only the data we need to a local inventory array:
+          inventory.push({
+            name: item.name,
+            id: item.id,
+            category: item.category,
+            color: item.color,
+            img: item.image['600px'],
+            price: price,
+            rarity: item.rarity
+          });
+        }
+      });
+      // Call the callback, returning our nicely formatted inventory
+      cb(null, inventory);
+    }
 	}
 }
 
@@ -113,62 +138,87 @@ function getUserInventory(steamid, refresh, force, cb) {
 			function doIt() {
 				// Remove existing cached inventory if it exists
 				utils.removeObjArr(userInventoryCache, 'steamid', steamid);
-				ET.ITrade.GetUserInventoryFromSteamId({
-					steam_id: steamid
-				}, (err, body) => {
-					if (err) {
-						cb(err);
-					} else {
-						if (body.status == 1) {
-							let localInv = [];
-							body.response.items.forEach(function(item) {
-								let banned;
-								// Check if the user item is banned by us in config:
-								if (config.bannedUserItems.length) {
-									config.bannedUserItems.forEach(function(bannedItem) {
-										if (item.name.toUpperCase().includes(bannedItem.toUpperCase())) {
-											banned = true;
-										}
-									});
-								}
-								
-								let price = item.suggested_price;
-								// Change the price based on the rates set in config
-								if (item.name == 'Skeleton Key') {
-									price = parseFloat(((price * config.rates.keyUserRate) / 100).toFixed(2));
-								} else {
-									price = parseFloat(((price * config.rates.otherUserRate) / 100).toFixed(2));
-								}
-								
-								// Check if the item price is allowed in config
-								if (price >= config.minMax.minUser && price <= config.minMax.maxUser && !banned) {
-									// If it's all good, push it to our local array
-									localInv.push({
-										name: item.name,
-										id: item.id,
-										category: item.category,
-										color: item.color,
-										img: item.image['600px'],
-										price: price,
-                    rarity: item.rarity
-									});
-								}
-							});
-							// Callback with our local formatted inventory
-							cb(null, {inventory: localInv});
-							// Cache the inventory for later
-							userInventoryCache.push({
-								steamid: steamid,
-								inventory: localInv,
-								updated: Date.now(),
-								cached: true
-							});
-						} else {
-							// Status wasn't 1
-							cb('An unknown error occurred fetching bot inventory');
-						}
-					}
-				});
+        
+        let demItems = [];
+        let page = 1;
+        
+        getIt();
+        
+        function getIt() {
+          ET.ITrade.GetUserInventoryFromSteamId({
+            steam_id: steamid,
+            page: page,
+            per_page: 1
+          }, (err, body) => {
+            if (err) {
+              cb(err);
+            } else {
+              if (body.status == 1) {
+                let items = body.response.items;
+                items.forEach(function(item) {
+                  demItems.push(item);
+                });
+
+                if (body.total_pages > body.current_page) {
+                  page += 1;
+                  getIt();
+                } else {
+                  nextStep(demItems);
+                }
+              } else {
+                // Status wasn't 1
+                cb('An unknown error occurred fetching bot inventory');
+              }
+            }
+          });
+        }
+        
+        function nextStep(demItems) {
+          let localInv = [];
+          demItems.forEach(function(item) {
+            let banned;
+            // Check if the user item is banned by us in config:
+            if (config.bannedUserItems.length) {
+              config.bannedUserItems.forEach(function(bannedItem) {
+                if (item.name.toUpperCase().includes(bannedItem.toUpperCase())) {
+                  banned = true;
+                }
+              });
+            }
+
+            let price = item.suggested_price;
+            // Change the price based on the rates set in config
+            if (item.name == 'WAX Key') {
+              price = parseFloat(((price * config.rates.keyUserRate) / 100).toFixed(2));
+            } else {
+              price = parseFloat(((price * config.rates.otherUserRate) / 100).toFixed(2));
+            }
+
+            // Check if the item price is allowed in config
+            if (price >= config.minMax.minUser && price <= config.minMax.maxUser && !banned) {
+              // If it's all good, push it to our local array
+              localInv.push({
+                name: item.name,
+                id: item.id,
+                category: item.category,
+                color: item.color,
+                img: item.image['600px'],
+                price: price,
+                rarity: item.rarity
+              });
+            }
+          });
+          // Callback with our local formatted inventory
+          cb(null, {inventory: localInv});
+          // Cache the inventory for later
+          userInventoryCache.push({
+            steamid: steamid,
+            inventory: localInv,
+            updated: Date.now(),
+            cached: true
+          });
+        }
+        
 			}
 		}
 	} else {
